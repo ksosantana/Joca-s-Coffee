0c43e2fb13d625ac03c9d4d85806bd34164bf43b
ksosantana <kristhiansantana94@gmail.com>
2023-11-09 19:45:15 -0400

    aplicação de midiaQuary

---
+++ botoes.js
@@ -0,0 +1,9 @@
+const botoesTitulos = document.querySelector('.botoes');
+const cafelist = document.getElementById('cafe');
+const sucoList = document.getElementById('suco');
+const tapiocasLis = document.getElementById('tapioca');
+const paoList = document.getElementById('pao');
+
+botoes.addEventListener('click', () => {
+    sucoList.classList.add('suco');
+  });
