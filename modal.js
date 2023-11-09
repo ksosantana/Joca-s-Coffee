bea769983c2af1b1d89beb81fb17d71258bb6614
kseraos <kseraos@outlook.com>
2023-11-09 16:03:47 -0400

    ajuste

--- modal.js
+++ modal.js
@@ -54,7 +54,7 @@ document.querySelectorAll('.conteudo-pai').forEach((divClick) => {
       // Exibir o modal
       modal.style.display = 'block';
     }
-<<<<<<< HEAD
+
   });
 });
 
@@ -86,28 +86,7 @@ function obterValorProduto(divClick) {
   const valorProduto = divClick.getAttribute('data-valor');
   return valorProduto || 'Valor não disponível';
 }
-=======
+
     
-})
-atualizarValor()
-
-const divClick =document.querySelector(".conteudo-pai")
-const modal=document.querySelector(".modal")
-const closeModal=document.querySelector(".fechar-modal")
-
-divClick.addEventListener('click', () =>{
-    modal.style.display="block"
-})
-closeModal.addEventListener('click', () => {
-    modal.style.display="none"
-})
-
-window.addEventListener('click', function(event){
- if(event.target===modal){
-    modal.style.display="none"
- }
-})
 
 
-
->>>>>>> 2d7baa8901fecdf86b2d1d7795c7e8b134552916
